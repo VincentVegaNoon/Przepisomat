d57e05d38d87732b419ab4cd60d84dfb57e4bdbd
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2024-01-26 08:53:53 +0100

    In progress

--- przepisomat/src/App.jsx
+++ przepisomat/src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
 import { IngredientSearchForm } from "./components/IngredientSearch/IngredientSearch";
 import { RecipeList } from "./components/RecipeList/RecipeList";
-import API_KEYS from "";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
 
+  const MOCK_API_RESPONSE = {
+    hits: [
+      {
+        recipe: {
+          label: "Chicken Parmesan",
+          image: "url_to_chicken_parmesan_image",
+          ingredientLines: [
+            "4 boneless chicken breast halves",
+            "1/2 cup breadcrumbs",
+            "1/4 cup grated Parmesan cheese",
+          ],
+          url: "url_to_recipe_details",
+        },
+      },
+      {
+        recipe: {
+          label: "Vegetable Stir Fry",
+          image: "url_to_vegetable_stir_fry_image",
+          ingredientLines: ["1 cup broccoli", "1/2 cup bell peppers", "1/4 cup carrots"],
+          url: "url_to_recipe_details",
+        },
+      },
+    ],
+  };
   const searchRecipes = async (ingredients) => {
-    const appId = API_KEYS.env.REACT_APP_EDAMAM_API_ID;
-    const appKey = API_KEYS.env.REACT_APP_EDAMAM_API_KEY;
-    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredients}&app_id=${appId}&app_key=${appKey}`;
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setRecipes(MOCK_API_RESPONSE.hits.map((hit) => hit.recipe));
   };
 
   return (
--- przepisomat/src/components/RecipeList/RecipeList.jsx
+++ przepisomat/src/components/RecipeList/RecipeList.jsx
@@ -4,7 +4,14 @@ export function RecipeList({ recipes }) {
   return (
     <div>
       {recipes.map((recipe) => (
-        <RecipeItem key={recipe.id} recipe={recipe} />
+        <div key={recipe.label}>
+          <h3>{recipe.label}</h3>
+          <img src={recipe.image} alt={recipe.label} />
+          <p>Ingredients: {recipe.ingredientLines.join(", ")}</p>
+          <a href={recipe.url} target="_blank" rel="noopener noreferrer">
+            View Recipe
+          </a>
+        </div>
       ))}
     </div>
   );
