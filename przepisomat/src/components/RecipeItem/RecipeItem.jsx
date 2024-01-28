7f9a4ca3918b3918a1c62154cc0a58aabdde1e00
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2024-01-28 22:47:42 +0100

    delete comment

--- przepisomat/src/components/RecipeItem/RecipeItem.jsx
+++ przepisomat/src/components/RecipeItem/RecipeItem.jsx
@@ -3,7 +3,6 @@ export function RecipeItem({ recipe }) {
     <div className="p-3 text-slate-50 font-medival">
       <h3>{recipe.title}</h3>
       <img src={recipe.image} />
-      {/* <p>Ingredients: {recipe.ingredientLines.join(", ")}</p> */}
       <a target="_blank" rel="noopener noreferrer">
         View Recipe
       </a>
